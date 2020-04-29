bbfae00c9a083bd7fbd06e7a18abc723855ee023
Matthew Schultz <mschult7@bingha>
2020-04-29 15:56:57 -0400

    add Node struct

--- main.cpp
+++ main.cpp
@@ -5,20 +5,26 @@
 
 using namespace std;
 
+struct Node{
+  int color;
+  int distance;
+  Node * parent;
+};
+
 int main(int argc, char* argv[]){
     int numOfNodes = 0, numOfEdges = 0;
 	int firstNode, secondNode;
 	string line;
-	
+
 	ifstream infile(argv[1]);
-	
+
 	getline(infile, line);
 	stringstream s(line);
 	s >> numOfNodes >> numOfEdges;
-	
+
 	while(getline(infile, line)){
 		break;
 	}
-	
+
 	cout << numOfNodes << ", " << numOfEdges << endl;
 }
