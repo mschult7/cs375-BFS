c4074becac6fa6274dc2fe532a21f2b725a459df
Jesse White <jwhite48@binghamton.edu>
2020-04-29 16:50:10 -0400

    changes

--- main.cpp
+++ main.cpp
@@ -2,29 +2,49 @@
 #include<fstream>
 #include<sstream>
 #include<string>
+#include<vector>
 
 using namespace std;
 
-struct Node{
-  int color;
-  int distance;
-  Node * parent;
+class Node{
+	public:
+		int index;
+		int color; //0 = white, 1 = grey, 2 = black
+		int distance;
+		Node * parent;
+		
+		Node(){
+			index = -1;
+			color = 0;
+			distance = -1;
+			parent = NULL;
+		}
 };
 
 int main(int argc, char* argv[]){
+	bool directed = false; //directed = true, undirected = false
     int numOfNodes = 0, numOfEdges = 0;
-	int firstNode, secondNode;
+	
+	int firstNode = 0, secondNode = 0;
 	string line;
 
 	ifstream infile(argv[1]);
 
 	getline(infile, line);
 	stringstream s(line);
+	s >> directed;
+	
+	getline(infile, line);
+	stringstream s2(line);
 	s >> numOfNodes >> numOfEdges;
+	
+	vector<vector<int> > adjList;
 
 	while(getline(infile, line)){
-		break;
+		stringstream s3(line);
+		
+		
 	}
 
-	cout << numOfNodes << ", " << numOfEdges << endl;
+	cout << directed << "\n" << numOfNodes << ", " << numOfEdges << endl;
 }
