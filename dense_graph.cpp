5f7184a96e281335e5e58befad25a91d82579011
Jesse White <jwhite48@binghamton.edu>
2020-05-04 14:08:53 -0400

    Beautified Code

--- dense_graph.cpp
+++ dense_graph.cpp
@@ -6,36 +6,37 @@
 
 using namespace std;
 
-int main(int argc, char* argv[]){
+int main(int argc, char * argv[]) {
     int numOfNodes = 100, numOfEdges = 0;
-	
-	int firstNode = 0, secondNode = -1;
-	vector<vector<int> > adjList; adjList.resize(numOfNodes);
-	
-	for(int i=1; i<numOfNodes; i++){
-		secondNode = i;
-		for(int j=0; j<secondNode; j++){
-			firstNode = j;
-			adjList[firstNode].push_back(secondNode); 
-			numOfEdges++;
-		}
-	}
-	
-	streambuf *psbuf, *backup;
-	ofstream o;
-	o.open(argv[1]);
-	backup = cout.rdbuf();
-	psbuf = o.rdbuf();
-	cout.rdbuf(psbuf);
-	
-	cout << "0" << endl;
-	cout << numOfNodes << " " << numOfEdges << endl;
-	for(int i=0; i<adjList.size(); i++){
-		for(int j=0; j<adjList[i].size(); j++){
-			cout << i << " " << adjList[i][j] << endl;
-		}
-	}
-	
-	cout.rdbuf(backup);
-	o.close();
+
+    int firstNode = 0, secondNode = -1;
+    vector < vector < int > > adjList;
+    adjList.resize(numOfNodes);
+
+    for (int i = 1; i < numOfNodes; i++) {
+        secondNode = i;
+        for (int j = 0; j < secondNode; j++) {
+            firstNode = j;
+            adjList[firstNode].push_back(secondNode);
+            numOfEdges++;
+        }
+    }
+
+    streambuf * psbuf, * backup;
+    ofstream o;
+    o.open(argv[1]);
+    backup = cout.rdbuf();
+    psbuf = o.rdbuf();
+    cout.rdbuf(psbuf);
+
+    cout << "0" << endl;
+    cout << numOfNodes << " " << numOfEdges << endl;
+    for (int i = 0; i < adjList.size(); i++) {
+        for (int j = 0; j < adjList[i].size(); j++) {
+            cout << i << " " << adjList[i][j] << endl;
+        }
+    }
+
+    cout.rdbuf(backup);
+    o.close();
 }
--- main.cpp
+++ main.cpp
@@ -8,308 +8,288 @@
 #include<queue>
 #include<chrono>
 
-using namespace std;
+class Node {
+    public:
+        int index;
+    	int color; //0 = white, 1 = grey, 2 = black
+    	int distance;
+    	Node * parent;
 
-class Node{
-public:
-  int index;
-  int color; //0 = white, 1 = grey, 2 = black
-  int distance;
-  Node * parent;
-
-  Node(){
-    index = -1;
-    color = 0;
-    distance = -1;
-    parent = NULL;
-  }
+    Node() {
+        index = -1;
+        color = 0;
+        distance = -1;
+        parent = NULL;
+    }
 };
 
-vector<pair<int,int> > BFS_list(vector<vector<int> >, vector<Node* >, int);
-vector<pair<int,int> > BFS_mat(vector<vector<int> >, vector<Node* >, int);
+vector < pair < int, int > > BFS_list(vector < vector < int > > , vector < Node * > , int);
+vector < pair < int, int > > BFS_mat(vector < vector < int > > , vector < Node * > , int);
 
-int main(int argc, char* argv[]){
-  vector<int> runtimeList; runtimeList.resize(100);
-  vector<int> runtimeMatrix; runtimeMatrix.resize(100);
+int main(int argc, char * argv[]) {
+    vector < int > runtimeList;
+    runtimeList.resize(100);
+    vector < int > runtimeMatrix;
+    runtimeMatrix.resize(100);
 
-  // int qez =0;
-  // for(int i=1;i<101;i++){
-  //   cout << qez << " " << i << endl;
-  //   if(i%2==0){
-  //     qez++;
-  //   }
-  //
-
-  //}
-  int printBFSTree = 1;
-  if(argv[3]!=NULL){
-    printBFSTree = stoi(argv[3]);
-  }
-  int isDemoGraph = 0;
-  if(argv[2]!=NULL){
-    isDemoGraph = stoi(argv[2]);
-  }
-  for(int tester=0;tester<1;tester++){
-    if(!printBFSTree){
-      cout << tester << "%" << endl;
+    int printBFSTree = 1;
+    if (argv[3] != NULL) {
+        printBFSTree = stoi(argv[3]);
     }
-
-    int directed = -1; //directed = 1, undirected = 0
-    int numOfNodes = -1, numOfEdges = -1;
-
-    int firstNode = -1, secondNode = -1;
-    string line;
-
-    ifstream infile(argv[1]);
-
-    getline(infile, line);
-    stringstream s1(line);
-    s1 >> directed;
-
-    getline(infile, line);
-    stringstream s2(line);
-    s2 >> numOfNodes >> numOfEdges;
-
-    vector<vector<int> > adjList; adjList.resize(numOfNodes);
-    vector<Node *> nodes; nodes.resize(numOfNodes);
-
-    vector<vector<int> > matrix; matrix.resize(numOfNodes);
-
-    for(int i=0; i<nodes.size(); i++){
-      nodes[i] = new Node();
-      matrix[i].resize(numOfNodes);
+    int isDemoGraph = 0;
+    if (argv[2] != NULL) {
+        isDemoGraph = stoi(argv[2]);
     }
-
-    while(getline(infile, line)){
-      stringstream s3(line);
-      s3 >> firstNode >> secondNode;
-
-      vector<int>::iterator indexFound;
-
-      if(directed==1){
-        matrix[firstNode][secondNode] = 1;
-
-        if(adjList[firstNode].size()==0){
-          adjList[firstNode].push_back(secondNode);
-        }else{
-          indexFound = find(adjList[firstNode].begin(), adjList[firstNode].end(), secondNode);
-
-          if(indexFound==adjList[firstNode].end()){
-            adjList[firstNode].push_back(secondNode);
-          }
-        }
-      }else{
-        matrix[firstNode][secondNode] = 1;
-        matrix[secondNode][firstNode] = 1;
-
-        if(adjList[firstNode].size()==0){
-          adjList[firstNode].push_back(secondNode);
-        }else{
-          indexFound = find(adjList[firstNode].begin(), adjList[firstNode].end(), secondNode);
-
-          if(indexFound==adjList[firstNode].end()){
-            adjList[firstNode].push_back(secondNode);
-          }
+    for (int tester = 0; tester < 1; tester++) { //CHANGE UPPERBOUND OF TESTER FOR MULTIPLE RUNS
+        if (!printBFSTree) {
+            cout << tester << "%" << endl;
         }
 
-        if(adjList[secondNode].size()==0){
-          adjList[secondNode].push_back(firstNode);
-        }else{
-          indexFound = find(adjList[secondNode].begin(), adjList[secondNode].end(), firstNode);
+        int directed = -1; //directed = 1, undirected = 0
+        int numOfNodes = -1, numOfEdges = -1;
 
-          if(indexFound==adjList[secondNode].end()){
-            adjList[secondNode].push_back(firstNode);
-          }
+        int firstNode = -1, secondNode = -1;
+        string line;
+
+        ifstream infile(argv[1]);
+
+        getline(infile, line);
+        stringstream s1(line);
+        s1 >> directed;
+
+        getline(infile, line);
+        stringstream s2(line);
+        s2 >> numOfNodes >> numOfEdges;
+
+        vector < vector < int > > adjList;
+        adjList.resize(numOfNodes);
+        vector < Node * > nodes;
+        nodes.resize(numOfNodes);
+
+        vector < vector < int > > matrix;
+        matrix.resize(numOfNodes);
+
+        for (int i = 0; i < nodes.size(); i++) {
+            nodes[i] = new Node();
+            matrix[i].resize(numOfNodes);
         }
-      }
+
+        while (getline(infile, line)) {
+            stringstream s3(line);
+            s3 >> firstNode >> secondNode;
+
+            vector < int > ::iterator indexFound;
+
+            if (directed == 1) {
+                matrix[firstNode][secondNode] = 1;
+
+                if (adjList[firstNode].size() == 0) {
+                    adjList[firstNode].push_back(secondNode);
+                } else {
+                    indexFound = find(adjList[firstNode].begin(), adjList[firstNode].end(), secondNode);
+
+                    if (indexFound == adjList[firstNode].end()) {
+                        adjList[firstNode].push_back(secondNode);
+                    }
+                }
+            } else {
+                matrix[firstNode][secondNode] = 1;
+                matrix[secondNode][firstNode] = 1;
+
+                if (adjList[firstNode].size() == 0) {
+                    adjList[firstNode].push_back(secondNode);
+                } else {
+                    indexFound = find(adjList[firstNode].begin(), adjList[firstNode].end(), secondNode);
+
+                    if (indexFound == adjList[firstNode].end()) {
+                        adjList[firstNode].push_back(secondNode);
+                    }
+                }
+
+                if (adjList[secondNode].size() == 0) {
+                    adjList[secondNode].push_back(firstNode);
+                } else {
+                    indexFound = find(adjList[secondNode].begin(), adjList[secondNode].end(), firstNode);
+
+                    if (indexFound == adjList[secondNode].end()) {
+                        adjList[secondNode].push_back(firstNode);
+                    }
+                }
+            }
+        }
+
+        vector < pair < int, int >> BFSTree_List;
+        vector < pair < int, int >> BFSTree_Mat;
+
+        auto start = chrono::steady_clock::now();
+        if (directed == 0) {
+            BFSTree_List = BFS_list(adjList, nodes, isDemoGraph);
+        } else if (directed == 1) {
+            BFSTree_List = BFS_list(adjList, nodes, isDemoGraph);
+        }
+        auto end = chrono::steady_clock::now();
+
+        if (printBFSTree) {
+            cout << "--------- BFS with adjacency list ---------" << endl;
+            for (int i = 0; i < nodes.size(); i++) {
+                cout << "Node: " << i << endl;
+                cout << "distance: " << nodes[i] - > distance << endl;
+                if (nodes[i] - > parent != NULL) {
+                    cout << "parent: " << nodes[i] - > parent - > index << endl;
+                } else {
+                    cout << "parent: NULL //This is the start Node" << endl;
+                }
+
+                cout << "edges: " << endl;
+                for (int j = 0; j < BFSTree_List.size(); j++) {
+                    if (BFSTree_List[j].first == i) {
+                        cout << BFSTree_List[j].second << endl;
+                    } else if (BFSTree_List[j].second == i && directed == 0) {
+                        cout << BFSTree_List[j].first << endl;
+                    }
+                }
+            }
+            cout << "Microseconds: " << chrono::duration_cast < chrono::microseconds > (end - start).count() << endl;
+        }
+
+        runtimeList[tester] = (chrono::duration_cast < chrono::microseconds > (end - start).count());
+
+        start = chrono::steady_clock::now();
+        if (directed == 0) {
+            BFSTree_Mat = BFS_mat(matrix, nodes, isDemoGraph);
+        } else if (directed == 1) {
+            BFSTree_Mat = BFS_mat(matrix, nodes, isDemoGraph);
+        }
+        end = chrono::steady_clock::now();
+
+        if (printBFSTree) {
+
+            cout << "---------------------------------------------" << endl;
+            cout << "--------- BFS with adjacency Matrix ---------" << endl;
+            for (int i = 0; i < nodes.size(); i++) {
+                cout << "Node: " << i << endl;
+                cout << "distance: " << nodes[i] - > distance << endl;
+                if (nodes[i] - > parent != NULL) {
+                    cout << "parent: " << nodes[i] - > parent - > index << endl;
+                } else {
+                    cout << "parent: NULL //This is the start Node" << endl;
+                }
+
+                cout << "edges: " << endl;
+                for (int j = 0; j < BFSTree_Mat.size(); j++) {
+                    if (BFSTree_Mat[j].first == i) {
+                        cout << BFSTree_Mat[j].second << endl;
+                    } else if (BFSTree_Mat[j].second == i && directed == 0) {
+                        cout << BFSTree_Mat[j].first << endl;
+                    }
+                }
+            }
+            cout << "Microseconds: " << chrono::duration_cast < chrono::microseconds > (end - start).count() << endl;
+            cout << "---------------------------------------------" << endl;
+        }
+        runtimeMatrix[tester] = (chrono::duration_cast < chrono::microseconds > (end - start).count());
+
+    }
+    int listcount = 0;
+    int nList = runtimeList.size();
+    double averagel = 0.0;
+    for (int i = 0; i < nList; i++) {
+        if (runtimeList[i] != 0) {
+            averagel += runtimeList[i];
+            listcount++;
+        }
+
+    }
+    averagel = averagel / listcount;
+
+    int nMat = runtimeMatrix.size();
+    int ncount = 0;
+    double averagem = 0.0;
+    for (int i = 0; i < nMat; i++) {
+        if (runtimeMatrix[i] != 0) {
+            averagem += runtimeMatrix[i];
+            ncount++;
+        }
+
+    }
+    averagem = averagem / ncount;
+
+    cout << "average adjList: " << averagel << endl;
+    cout << "average adjMatrix: " << averagem << endl;
+}
+
+vector < pair < int, int > > BFS_mat(vector < vector < int > > matrix, vector < Node * > nodes, int start) {
+    for (Node * u: nodes) {
+        u - > color = 0; //white
+        u - > distance = -1; //infinity
+        u - > parent = NULL;
+    }
+    vector < pair < int, int > > edgesIncluded;
+    queue < Node * > q;
+    Node * s = nodes[start];
+    s - > color = 1;
+    s - > distance = 0;
+    s - > parent = NULL;
+    s - > index = start;
+    q.push(s);
+
+    Node * curr;
+    while (!q.empty()) {
+        curr = q.front();
+        q.pop(); //remove current node from queue
+
+        for (int i = 0; i < matrix.size(); i++) { //check nodes adajcent to current node
+            //for(int j = 0; j > mat[][].size(); j++){}
+
+            if (matrix[curr - > index][i] == 1 && nodes[i] - > color == 0) { //if node is adjacent and not visited
+                nodes[i] - > color = 1; //mark node (grey)
+                nodes[i] - > distance = curr - > distance + 1;
+                nodes[i] - > parent = curr;
+                nodes[i] - > index = i;
+                pair < int, int > BFSedge(curr - > index, i);
+                edgesIncluded.push_back(BFSedge);
+                q.push(nodes[i]); //add to queue
+            }
+        }
+        curr - > color = 2; //current node has been explored (black)
+
     }
 
-    /*for(int i=0; i<adjList.size(); i++){
-    cout << "Node: " << i << endl;
-    for(int j=0; j<adjList[i].size(); j++){
-    cout << adjList[i][j] << endl;
-  }
-}*/
-
-vector<pair<int,int>>BFSTree_List;
-vector<pair<int,int>>BFSTree_Mat;
-
-auto start = chrono::steady_clock::now();
-if(directed==0){
-  BFSTree_List = BFS_list(adjList,nodes,isDemoGraph);
-} else if(directed==1){
-  BFSTree_List = BFS_list(adjList,nodes,isDemoGraph);
+    return edgesIncluded;
 }
-auto end = chrono::steady_clock::now();
 
-
-
-if(printBFSTree){
-cout << "--------- BFS with adjacency list ---------" << endl;
-  for(int i=0; i<nodes.size(); i++){
-    cout << "Node: " << i << endl;
-    cout << "distance: " << nodes[i]->distance << endl;
-    if(nodes[i]->parent != NULL){
-      cout << "parent: " << nodes[i]->parent->index << endl;
-    } else {
-      cout << "parent: NULL //This is the start Node" << endl;
+vector < pair < int, int > > BFS_list(vector < vector < int > > adjList, vector < Node * > vertex, int startNode) {
+    for (Node * u: vertex) {
+        u - > color = 0; //white
+        u - > distance = -1; //infinity
+        u - > parent = NULL;
     }
-
-    cout << "edges: " << endl;
-    for(int j=0; j<BFSTree_List.size(); j++){
-      if(BFSTree_List[j].first == i ){
-        cout << BFSTree_List[j].second << endl;
-      }else if ( BFSTree_List[j].second == i && directed==0 ) {
-        cout << BFSTree_List[j].first << endl;
-      }
+    vector < pair < int, int > > edgesIncluded;
+    Node * s = vertex[startNode];
+    s - > color = 1; //grey
+    s - > distance = 0; //origin
+    s - > parent = NULL;
+    s - > index = startNode;
+    queue < Node * > q;
+    q.push(s);
+    Node * x;
+    Node * v;
+    while (!q.empty()) {
+        x = q.front();
+        q.pop();
+        for (int edge: adjList[x - > index]) {
+            v = vertex[edge];
+            if (v - > color == 0) { //white
+                v - > color = 1; //grey
+                v - > distance = x - > distance + 1;
+                v - > parent = x;
+                v - > index = edge;
+                pair < int, int > BFSedge(x - > index, v - > index);
+                edgesIncluded.push_back(BFSedge);
+                q.push(v);
+            }
+        }
+        x - > color = 2; //black
     }
-  }
-  cout << "Microseconds: " << chrono::duration_cast<chrono::microseconds>(end - start).count() << endl;
+    return edgesIncluded;
 }
-
-runtimeList[tester] = (chrono::duration_cast<chrono::microseconds>(end - start).count());
-
-
-start = chrono::steady_clock::now();
-if(directed==0){
-  BFSTree_Mat = BFS_mat(matrix,nodes,isDemoGraph);
-} else if(directed==1){
-  BFSTree_Mat = BFS_mat(matrix,nodes,isDemoGraph);
-}
-end = chrono::steady_clock::now();
-
-if(printBFSTree){
-
-  cout << "---------------------------------------------" << endl;
-  cout << "--------- BFS with adjacency Matrix ---------" << endl;
-  for(int i=0; i<nodes.size(); i++){
-    cout << "Node: " << i << endl;
-    cout << "distance: " << nodes[i]->distance << endl;
-    if(nodes[i]->parent != NULL){
-      cout << "parent: " << nodes[i]->parent->index << endl;
-    } else {
-      cout << "parent: NULL //This is the start Node" << endl;
-    }
-
-    cout << "edges: " << endl;
-    for(int j=0; j<BFSTree_Mat.size(); j++){
-      if(BFSTree_Mat[j].first == i ){
-        cout << BFSTree_Mat[j].second << endl;
-      }else if ( BFSTree_Mat[j].second == i && directed==0 ) {
-        cout << BFSTree_Mat[j].first << endl;
-      }
-    }
-  }
-  cout << "Microseconds: " << chrono::duration_cast<chrono::microseconds>(end - start).count() << endl;
-  cout << "---------------------------------------------" << endl;
-}
-runtimeMatrix[tester] = (chrono::duration_cast<chrono::microseconds>(end - start).count());
-
-
-}
-int listcount = 0;
-int nList = runtimeList.size();
-double averagel = 0.0;
-for(int i=0;i<nList;i++){
-  if(runtimeList[i]!=0){
-    averagel += runtimeList[i];
-    listcount++;
-  }
-
-}
-averagel = averagel/listcount;
-
-int nMat = runtimeMatrix.size();
-int ncount = 0;
-double averagem = 0.0;
-for(int i=0;i<nMat;i++){
-  if(runtimeMatrix[i]!=0){
-    averagem += runtimeMatrix[i];
-    ncount++;
-  }
-
-}
-averagem = averagem/ncount;
-
-
-cout << "average adjList: " << averagel << endl;
-cout << "average adjMatrix: " << averagem  << endl;
-}
-
-vector<pair<int,int> > BFS_mat(vector<vector<int> > matrix, vector<Node*> nodes, int start){
-  for(Node * u: nodes){
-    u->color = 0;//white
-    u->distance = -1;//infinity
-    u->parent = NULL;
-  }
-  vector<pair<int,int> > edgesIncluded;
-  queue<Node*> q;
-  Node * s = nodes[start];
-  s->color = 1;
-  s->distance = 0;
-  s->parent = NULL;
-  s->index = start;
-  q.push(s);
-
-  Node* curr;
-  while(!q.empty()){
-    curr = q.front();
-    q.pop(); //remove current node from queue
-
-    for(int i = 0; i < matrix.size(); i++){ //check nodes adajcent to current node
-      //for(int j = 0; j > mat[][].size(); j++){}
-
-      if(matrix[curr->index][i] == 1 && nodes[i]->color == 0){ //if node is adjacent and not visited
-        nodes[i]->color = 1;    //mark node (grey)
-        nodes[i]->distance = curr->distance + 1;
-        nodes[i]->parent = curr;
-        nodes[i]->index = i;
-        pair<int,int> BFSedge(curr->index,i);
-        edgesIncluded.push_back(BFSedge);
-        q.push(nodes[i]);   //add to queue
-      }
-    }
-    curr->color = 2; //current node has been explored (black)
-
-  }
-
-  return edgesIncluded;
-}
-
-vector<pair<int,int> > BFS_list(vector<vector<int> > adjList, vector<Node* > vertex, int startNode){
-  for(Node * u: vertex){
-    u->color = 0;//white
-    u->distance = -1;//infinity
-    u->parent = NULL;
-  }
-  vector<pair<int,int> > edgesIncluded;
-  Node * s = vertex[startNode];
-  s->color = 1;//grey
-  s->distance = 0;//origin
-  s->parent = NULL;
-  s->index = startNode;
-  queue<Node *> q;
-  q.push(s);
-  Node * x;
-  Node * v;
-  while(!q.empty()){
-    x = q.front();
-    q.pop();
-    for(int edge: adjList[x->index]){
-      v = vertex[edge];
-      if(v->color==0){//white
-        v->color=1;//grey
-        v->distance = x->distance + 1;
-        v->parent = x;
-        v->index = edge;
-        pair<int,int> BFSedge(x->index,v->index);
-        edgesIncluded.push_back(BFSedge);
-        q.push(v);
-      }
-    }
-    x->color = 2;//black
-  }
-  return edgesIncluded;
-}
-
-//it = find (myvector.begin(), myvector.end(), 30);
